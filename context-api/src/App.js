8b88d2575d4b40bb058bd750318a148287626578
Germano Lira <dolan.3@hotmail.com>
2021-07-01 01:10:56 -0300

    context api

---
+++ context-api/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import CountProvider from './context/Count'
+
+import Counter from './components/Counter'
+import Mirror from './components/Mirror'
+
+function App() {
+  return (
+    <CountProvider>
+      <div>
+        <Counter />
+
+        <hr />
+
+        <Mirror />
+      </div>
+    </CountProvider>
+  )
+}
+
+export default App;
