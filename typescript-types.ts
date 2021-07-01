de3defb0dde12f7e641351de2b696669be1b8d57
Germano Lira <dolan.3@hotmail.com>
2021-06-30 23:35:08 -0300

    typescript types shorts

---
+++ typescript-types.ts
@@ -0,0 +1,14 @@
+type Icon = '🌮' | '🍟' | '🍦' | '🥑';
+// type Icon = 'gray' | 'white' | 'blue' | 'red';
+type Size = 'xl' | 'lg' | 'md' | 'sm';
+
+type SizedIcon = `${Size}-${Icon}`;
+
+const magic:SizedIcon = ''
+
+// type Color = 'red' | 'green' | 'blue' | 'yellow';
+// type NumberVariation = '.100' | '.200' | '.300' | '.400';
+
+// type ColorNumber = `${Color}${NumberVariation}`;
+
+// const magic:ColorNumber = ''
